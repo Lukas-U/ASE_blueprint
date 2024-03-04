42fb03d71fd2c67b455f7c7bdbdfc2b4080eb9a6
LukasU <lukasu@gmx.at>
2024-03-04 14:26:57 +0100

    Initial Commit

---
+++ src/main/java/com/tuwien/ASE_blueprint/AseBlueprintApplication.java
@@ -0,0 +1,13 @@
+package com.tuwien.ASE_blueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseBlueprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AseBlueprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuwien/ASE_blueprint/AseBlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuwien.ASE_blueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AseBlueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
